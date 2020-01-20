fa65ee0d5ac90380ff15fbff75fd634568a7f618
butler-yun <myung5722@naver.com>
2020-01-20 22:40:00 +0900

    Changing color or Mode and Brush Size

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 const canvas = document.querySelector('#jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+const range = document.querySelector('#jsRange');
+const buttonMode = document.querySelector('#jsMode');
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2d2d2d";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = '#2d2d2d';
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 const stopPainting = () => {
     painting = false;
@@ -30,12 +38,36 @@ const onMouseMove = (event) => {
     }
 }
 
-const onMouseDown = (event) => {
-    painting = true;
-}
 
 const onMouseUp = (event) => {
     stopPainting();
+}
+
+const handleClickColor = (event) => {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+} 
+
+const handleRangeChange = (event) => {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+const hangleChangeMode = () => {
+    if (filling === true) {
+        filling = false;
+        buttonMode.innerText = 'Fill';
+    } else {
+        filling = true;
+        buttonMode.innerText = 'Paint';
+    }
+}
+
+const handleFillCanvas = () => {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
 }
 
 if (canvas) {
@@ -43,4 +75,15 @@ if (canvas) {
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleFillCanvas);
+}
+
+Array.from(colors).forEach((color) => color.addEventListener('click', handleClickColor));
+
+if (range) {
+    range.addEventListener('input', handleRangeChange);
+}
+
+if (buttonMode) {
+    buttonMode.addEventListener('click', hangleChangeMode)
 }
